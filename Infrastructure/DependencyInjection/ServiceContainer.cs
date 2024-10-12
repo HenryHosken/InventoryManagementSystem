5b2e102836a638531f46a1331f70bd1d8bf6d74e
henry hosken <henry_hosken12@outlook.com>
2024-10-11 21:42:06 -0300

    Adicionar arquivos de projeto.

---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,45 @@
+﻿using Application.Extension.Identity;
+using Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(o => o.UseSqlServer(config.GetConnectionString("Default")), ServiceLifetime.Scoped);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            }).AddIdentityCookies();
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthorizationBuilder()
+                .AddPolicy("AdminitrationPolicy", adp =>
+                {
+                    adp.RequireAuthenticatedUser();
+                    adp.RequireRole("Admin", "Manager");
+                })
+                .AddPolicy("YserPolicy", adp =>
+                {
+                    adp.RequireAuthenticatedUser();
+                    adp.RequireRole("User");
+                });
+            return services;
+        }
+    }
+}
